2902571d1505d57a7961da653b5ca8182c472777
fayjulnahid <fayjulnahid@gmail.com>
2021-11-22 16:03:59 +0600

    Homepage

--- HIS/articles/urls.py
+++ HIS/articles/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+app_name = 'articles'
 
 urlpatterns = [
-    url(r'^$',views.article_list),
+    url(r'^$', views.article_list, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
