5ae25c4323673b70e57d189448faa596f89a4ed6
fayjulnahid <fayjulnahid@gmail.com>
2021-11-22 11:53:31 +0600

    Homepage

---
+++ HIS/HIS/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^articles/',include('articles.urls')),
+    url(r'^about/$',views.about),
+    url(r'^$',views.homepage)
+]
+
+urlpatterns += staticfiles_urlpatterns()
